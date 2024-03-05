ec000dfdd6fe873b8b79b7892deb1c81978cb0e3
TheUltima <xxdeatjerzxx@gmail.com>
2024-03-05 04:41:53 -0500

    Add prompt for number of squares on drawing board
    A custom board will be made with the number inputted into the prompt. A
    minimum and maximum was set, along with handling any inputs that are not
    numbers. The board however, will not be sized up (problem for low
    numbers), still have to figure that out.

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const mainDrawingBoard = document.querySelector(".drawing-board");
-let rows = 32;
-let drawing = true;
-// let isMouseDown = false;
-
-// document.onmousedown = () => {
-//   isMouseDown = true;
-// };
-// document.onmouseup = () => {
-//   isMouseDown = false;
-// };
+let rows = prompt("How many squares should your board have?", 16);
+if (isNaN(rows) || rows < 16 || rows > 100) {
+  rows = prompt("Please type a valid number ranging from 16 to 100.");
+}
 
 createDrawingBoard();
 
@@ -28,13 +22,14 @@ function createDrawingBoard() {
   attachEventListeners();
 }
 
+let dragging = false;
+
 function attachEventListeners() {
   const individualSquare = document.querySelectorAll(".square");
 
   individualSquare.forEach((square) => {
     square.addEventListener("mouseover", (event) => {
       if (event.buttons === 1) {
-        console.log(event);
         square.classList.add("drawn");
         // square.style["background-color"] = `rgb(5,4,2)`;
       }
